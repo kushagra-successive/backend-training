41781d5dcdfbc3495129de96dfccf2fc2af34f10
kushagra-successive <kushagra.bajpai@successive.tech>
2024-05-09 10:46:20 +0530

    Assignment9

--- Assignment9/src/app.js
+++ Assignment9/src/app.js
@@ -1,7 +1,11 @@
+require("dotenv").config({
+  path: "./Assignment9/.env",
+});
 const mongoDB = require("./config/server");
 const serverSetup = require("./server");
+const PORT = process.env.PORT || 4000;
 mongoDB()
   .then(() => {
-    serverSetup().listen(3000, () => console.log(`listening at 3000`));
+    serverSetup().listen(PORT, () => console.log(`listening at ${PORT}`));
   })
   .catch((err) => console.log(err.message));
