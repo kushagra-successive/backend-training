477b561876bcbda8ac6a12927635f4f0593c9459
Kushagra <162109764+kushagra-successive@users.noreply.github.com>
2024-05-23 12:14:45 +0530

    Delete dist directory

--- dist/controllers/validateControl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validateControl = (req, res) => {
-    res.json({ message: `User Authenticated:${req.query.name}` });
-};
-exports.default = validateControl;
