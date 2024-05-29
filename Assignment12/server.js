7f0ef79fae66d1e7e116d56addc5de5f58618134
kushagra-successive <kushagra.bajpai@successive.tech>
2024-05-29 08:13:03 +0530

    inprogress

---
+++ Assignment12/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const StudentRouter=require('./routes/studentRouter');
+const helmet = require("helmet");
+const app = express();
+
+const server = () => {
+  app.use(express.json());
+  app.use(helmet());
+  app.use(StudentRouter);
+  return app;
+};
+
+module.exports = server;
